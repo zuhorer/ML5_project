acc27b670e6780da1d31b18854d425e9a12c98fa
unknown <yashbhadauria@gmail.com>
2020-06-08 23:34:04 +0530

    'Version0.0'

---
+++ public/sketch.js
@@ -0,0 +1,41 @@
+let bernard;
+let classifier;
+let socket;
+let className='';
+function modelLoaded() {
+  console.log('Model Loaded!');
+	classifier.classify(gotResults);
+}
+
+function gotResults(err,results){
+	if (err){
+		console.error(error);
+	}else{
+		
+		className= results[0].label;
+		classifier.classify(gotResults);
+}
+}
+
+
+/*function imageReady(){
+	image(bernard,0,0,width,height);
+}*/
+
+function setup() {
+	createCanvas(600,420);
+	bernard= createCapture(VIDEO);
+	bernard.hide();
+	socket = io.connect('http://localhost:3000');
+	classifier = ml5.imageClassifier('MobileNet',bernard, modelLoaded);
+	//bernard= createCapture('images/st-bernard-dog-alps.jpg',imageReady);
+
+  }
+
+function draw(){
+	image(bernard,0,0);
+	fill(0);
+	textSize(64);
+	text(className,10,height-100);
+
+}
---
+++ server.js
@@ -0,0 +1,99 @@
+//intailizing the packages
+const express= require('express')
+const hbs=require('hbs');
+const fs=require('fs');
+const socket=require('socket.io')
+var app=express();
+
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname+ '/public'));
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+const port=process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+});
+
+// Create gradient
+/*var grd = ctx.createLinearGradient(0, 0, 200, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
+
+*/
+
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = now
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err){
+      console.log('unable to append file')
+    }
+  })
+  next()
+});
+/*
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs',{
+
+  })
+});*/
+
+hbs.registerHelper('getcurrentyear',()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('BOLD',(text)=>{
+  return text.toUpperCase()
+});
+
+
+
+app.get('/Home',(req,res)=>{
+  //res.send('Hello Express');
+  res.render('home.hbs',{
+    pagetitle:'Home',
+    pagecontent:'vishnupriya saree collection',
+
+  })
+});
+
+app.get('/ProductIdentifier',(req,res)=>{
+  res.render('ProductIdentifier.hbs',{
+    pagetitle: 'Product video searcher',
+    PageMessage: 'Point towards the item you want to buy'
+  })
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pagetitle:'about page',
+    welcomeMessage:'welcome to our website'
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    response: 'The call failed.'
+  })
+});
+
+server.listen(port,()=>{
+  console.log("server is up on port"+port)
+});
