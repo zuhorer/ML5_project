8543d56a97a9707b21c1db5d131837c93f603785
unknown <yashbhadauria@gmail.com>
2020-06-10 01:19:52 +0530

    'for_mobile_phone'

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,17 @@
+var capture;
+let switchFlag = false;
+let switchBtn;
+
+var options = {
+     video: {
+
+         facingMode: {
+          exact: "user"
+        }
+     }
+   };
+
+
 let bernard;
 let classifier;
 let className='';
@@ -24,8 +38,19 @@ function gotResults(err,results){
 
 function setup() {
 	createCanvas(600,420);
-	bernard= createCapture(VIDEO);
-  bernard.elt.setAttribute('playsinline', '');
+  bernard= createCapture(options);
+
+  switchBtn = createButton('Switch Camera');
+  switchBtn.position(19, 19);
+  switchBtn.mousePressed(switchCamera);
+
+
+
+
+
+
+
+
 	bernard.hide();
 
 
@@ -33,9 +58,53 @@ function setup() {
 	//bernard= createCapture('images/st-bernard-dog-alps.jpg',imageReady);
 
   }
+  function switchCamera()
+  {
+    switchFlag = !switchFlag;
+    stopCapture();
+    if(switchFlag==true)
+    {
+     capture.remove();
+     options = {
+       video: {
+           facingMode: {
+            exact: "environment"
+          }
+       }
+     };
+
+    }
+    else
+    {
+     capture.remove();
+     options = {
+       video: {
+           facingMode: {
+            exact: "user"
+          }
+       }
+     };
+    }
+    capture = createCapture(options);
+  }
+
+  function stopCapture() {
+    let stream = capture.elt.srcObject;
+    let tracks = stream.getTracks();
+
+    tracks.forEach(function(track) {
+      track.stop();
+    });
+
+    capture.elt.srcObject = null;
+  }
+
+
 
 function draw(){
-	image(bernard,0,0);
+  translate(width,0); // move to far corner
+  scale(-1.0,1.0);    // flip x-axis backwards
+  image(bernard, 0, 0, width, height); //video on canvas, position, dimensions
 	fill(0);
 	textSize(64);
 	text(className,10,height-100);
